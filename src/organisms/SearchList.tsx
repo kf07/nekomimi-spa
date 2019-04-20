d6604f1a0894d4437063fb43ca7f9561fb33b9f8
kango furusu <furusu0715@gmail.com>
2019-04-20 17:30:16 +0900

    検索の実装

--- src/organisms/SearchList.tsx
+++ src/organisms/SearchList.tsx
@@ -1,9 +1,10 @@
-import { List,ListItem,ListSubheader } from '@material-ui/core';
+import { List,ListItem,ListItemText,ListSubheader } from '@material-ui/core';
 import React from 'react';
+import styles from './SearchList.module.css';
 
 interface Props {
   query: string;
-  list: any[];
+  list: gapi.client.youtube.SearchResult[];
 }
 
 const SearchList = (props:Props) => {
@@ -13,11 +14,19 @@ const SearchList = (props:Props) => {
       subheader={<ListSubheader>{`${query}の検索結果`}</ListSubheader>}
       dense
     >
-      {list.map(item => (
-        <ListItem key={item} button divider>
-          {item}
+      {list.map(item => {
+        const { etag,snippet } = item;
+        return (
+        <ListItem key={etag!} button divider>
+          <img
+            className={styles.thumbnail}
+            src={snippet!.thumbnails!.default!.url}
+            alt={snippet!.title}
+          />
+          <ListItemText primary={snippet!.title} />
         </ListItem>
-      ))}
+        );
+      })}
     </List>
   );
 };
--- src/templates/Root.tsx
+++ src/templates/Root.tsx
@@ -1,18 +1,29 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import styles from './Root.module.css';
 import SearchBar from "../organisms/SearchBar";
 import SearchList from "../organisms/SearchList";
+import ApiClient from '../utils/ApiClient';
 
 const RootTemplate = () => {
   const [query,setQuery] = useState('');
+  const [list,setList] = useState<gapi.client.youtube.SearchResult[]>([]);
   const handleChange = useCallback((q: string) => {
     setQuery(q);
   },[]);
+  useEffect(()=>{
+    const searchList = async () => {
+     const { items } = await ApiClient.search(query);
+     setList(items!);
+    };
+    if(query.trim() !== '') {
+      searchList()
+    }
+  },[query]);
   return (
     <>
       <SearchBar onChange={handleChange}/>
       <div className={styles.content}>
-        <SearchList query={query} list={[1,2,3,4,5]} />
+        <SearchList query={query} list={list}     />
       </div>
     </>
   )
