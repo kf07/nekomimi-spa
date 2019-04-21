ce2ba06b55a64837ca435b925842bbfc61c4e09f
kango furusu <furusu0715@gmail.com>
2019-04-21 17:53:58 +0900

    ローディング、エラー実装

--- src/organisms/SearchList.tsx
+++ src/organisms/SearchList.tsx
@@ -1,18 +1,45 @@
-import { List,ListItem,ListItemText,ListSubheader } from '@material-ui/core';
+import { List,ListItem,ListItemText,ListSubheader,Typography } from '@material-ui/core';
+import ErrorIcon from '@material-ui/icons/Error';
 import React,{ useCallback } from 'react';
 import { RouteComponentProps, withRouter } from "react-router-dom";
 import styles from './SearchList.module.css';
+import ReactLoading from 'react-loading';
 
 interface Props extends RouteComponentProps{
+  isLoading: boolean;
+  isError: boolean;
   query: string;
   list: gapi.client.youtube.SearchResult[];
 }
 
 const SearchList = (props:Props) => {
-  const { query,list, history } = props;
+  const { isLoading,isError,query,list, history } = props;
   const createHandleClick = useCallback((videoId: string) => {
     return ()=> history.push(`/watch/${videoId}`);
   },[]);
+  if(list.length === 0) {
+    return (
+      <div className={styles.content}>
+        <ErrorIcon fontSize="large" />
+        <Typography variant="body1">動画が見つかりませんでした。</Typography>
+      </div>
+    )
+  }
+  if(isError) {
+    return (
+      <div className={styles.content}>
+        <ErrorIcon fontSize="large" />
+        <Typography variant="body1">動画リストの取得に失敗しました</Typography>
+      </div>
+    )
+  }
+  if(isLoading) {
+    return (
+      <div className={styles.content}>
+        <ReactLoading type="bubbles" color="#999" />
+      </div>
+    );
+  }
   return (
     <List
       subheader={<ListSubheader>{`${query}の検索結果`}</ListSubheader>}
--- src/templates/Root.tsx
+++ src/templates/Root.tsx
@@ -1,29 +1,45 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import debounce from 'lodash.debounce';
+import React, {useState, useCallback} from 'react';
 import styles from './Root.module.css';
 import SearchBar from "../organisms/SearchBar";
 import SearchList from "../organisms/SearchList";
 import ApiClient from '../utils/ApiClient';
 
 const RootTemplate = () => {
-  const [query,setQuery] = useState('');
-  const [list,setList] = useState<gapi.client.youtube.SearchResult[]>([]);
-  const handleChange = useCallback((q: string) => {
-    setQuery(q);
-  },[]);
-  useEffect(()=>{
-    const searchList = async () => {
-     const { items } = await ApiClient.search(query);
-     setList(items!);
-    };
-    if(query.trim() !== '') {
-      searchList()
-    }
-  },[query]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const [list, setList] = useState<gapi.client.youtube.SearchResult[]>([]);
+  const searchList = useCallback(
+    debounce(async (q: string) => {
+      try {
+        setIsError(false);
+        setIsLoading(true);
+        if (q.trim() !== '') {
+          const {items} = await ApiClient.search(q);
+          setQuery(q);
+          setList(items!)
+        } else {
+          setList([]);
+        }
+      } catch (e) {
+        setIsError(true)
+      } finally {
+        setIsLoading(false);
+      }
+    }, 500),
+    []
+  );
   return (
     <>
-      <SearchBar onChange={handleChange}/>
+      <SearchBar onChange={searchList}/>
       <div className={styles.content}>
-        <SearchList query={query} list={list}     />
+        <SearchList
+          isLoading={isLoading}
+          isError={isError}
+          query={query}
+          list={list}
+        />
       </div>
     </>
   )
